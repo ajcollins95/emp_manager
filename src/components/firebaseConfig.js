d827a1433e24f2f3315d76bc16087f7b83af695c
Aaron Collins <aa.collins.95@gmail.com>
2019-06-14 20:14:42 -0400

    B&D part 1

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBu7hNwwoCsxSSR1Wq-HEOdMy0cKNLZfzA",
+  authDomain: "vuefs-prod-7d3df.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-7d3df.firebaseio.com",
+  projectId: "vuefs-prod-7d3df",
+  storageBucket: "vuefs-prod-7d3df.appspot.com",
+  messagingSenderId: "680097695841",
+  appId: "1:680097695841:web:83a6eb2a96a5c54e"
+}
